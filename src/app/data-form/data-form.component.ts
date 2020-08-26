68890c5b13714b80b83b060c7668f053efffb84d
junioralves00 <junioralves00@gmail.com>
2020-08-26 09:38:32 -0300

    aula95

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -1,5 +1,7 @@
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-form',
@@ -10,13 +12,56 @@ export class DataFormComponent implements OnInit {
 
   formulario: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
-    this.formulario = new FormGroup({
+    /*this.formulario = new FormGroup({
       nome: new FormControl(null),
       email: new FormControl(null)
+    });*/
+
+    this.formulario = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email]]
     });
   }
+  //[null, Validators.required, Validators.minLength(3), Validators.maxLength(20)]
 
+  onSubmit() {
+    this.http.post('https://httpbin.org/post', JSON.stringify(this.formulario.value))
+      .pipe(map(res => res))
+      .subscribe(dados => {
+        console.log(dados);
+        //reseta o form
+        this.formulario.reset();
+      },
+        (error: any) => alert('erro'));
+  }
+
+  resetar() {
+    this.formulario.reset();
+
+  }
+
+  verificaValidTouched(campo) {
+
+    return !this.formulario.get(campo).valid && this.formulario.get(campo).touched;
+  }
+
+  verificaEmailInvalido() {
+    let campoEmail = this.formulario.get('email');
+    if (campoEmail.errors) {
+      return campoEmail.errors['email'] && campoEmail.touched;
+    }
+  }
+
+  aplicaCssErro(campo) {
+    return {
+      'class.has-error': this.verificaValidTouched(campo),
+      'class.has-feedback': this.verificaValidTouched(campo)
+    }
+  }
 }
--- src/app/data-form/data-form.module.ts
+++ src/app/data-form/data-form.module.ts
@@ -1,10 +1,12 @@
-
-
 import { DataFormComponent } from './data-form.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { SharedModule } from './../shared/shared.module';
+
+
 
 @NgModule({
   imports: [
@@ -12,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule
   ],
   declarations: [
     DataFormComponent
